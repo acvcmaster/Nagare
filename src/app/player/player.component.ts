46bdcfd4138ec7ad4a0b5670f9e41cbb0fc07dc0
acvcmaster <acvcnos@hotmail.com>
2020-02-10 00:53:08 -0300

    Shuffling now works

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 import { Playlist } from './playlist/playlist.model';
 import { PlaylistService } from './playlist/playlist.service';
 import { Subscription } from 'rxjs';
@@ -9,16 +9,18 @@ import { Song } from './song/song.model';
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit, OnDestroy {
+export class PlayerComponent implements OnInit, OnDestroy, AfterViewInit {
+  dblClickFlag: boolean;
 
   constructor(private playlistService: PlaylistService) { }
 
-  @ViewChild('search', {static: false}) search: ElementRef;
-  @ViewChild('audio', {static: false}) audio: ElementRef;
-  shuffle = true;
+  @ViewChild('search', {static: false}) searchInput: ElementRef;
+  @ViewChild('audio', {static: false}) audioElement: ElementRef;
+  shuffle = false;
   playlists: Playlist[];
   subscriptions = new Subscription();
   currentPlaylist: Playlist;
+  currentPlaylistUnshuffled: Playlist;
   currentSong: Song;
   currentPlayingSong: Song;
   currentSongIndex = 0;
@@ -34,6 +36,7 @@ export class PlayerComponent implements OnInit, OnDestroy {
       if (playlist && playlist.length) {
         this.playlists = playlist;
         this.currentPlaylist = this.playlists[0];
+        this.currentPlaylistUnshuffled = JSON.parse(JSON.stringify(this.currentPlaylist));
         this.currentSong = this.currentPlaylist.songs[this.currentSongIndex];
         this.getFilteredPlaylist();
       }
@@ -44,24 +47,25 @@ export class PlayerComponent implements OnInit, OnDestroy {
     this.subscriptions.unsubscribe();
   }
 
+  ngAfterViewInit() {
+    this.audioElement.nativeElement.onended = () => {
+      this.changeSong();
+    };
+  }
+
   changeSong(previous?: boolean) {
     if (this.currentPlaylist && this.currentPlaylist.songs && this.currentPlaylist.songs.length) {
       this.currentSongIndex = (this.currentSongIndex + (previous ? -1 : 1)) % this.currentPlaylist.songs.length;
       this.currentSongIndex = this.currentSongIndex >= 0 ? this.currentSongIndex :
         (this.currentPlaylist.songs.length + this.currentSongIndex);
-      this.selectSong(this.currentPlaylist.songs[this.currentSongIndex], true);
+      this.selectSong(this.currentPlaylist.songs[this.currentSongIndex]);
+      this.play(true);
     }
   }
 
-  selectSong(song: Song, setPlaying?: boolean) {
+  selectSong(song: Song) {
     this.currentSong = song;
     this.currentSongIndex = this.currentPlaylist.songs.findIndex(item => item === song);
-    if (setPlaying) {
-      this.playing = false;
-      this.audio.nativeElement.pause();
-      this.currentPlayingSong = null;
-      this.play();
-    }
   }
 
   getFilteredPlaylist(filter?: string) {
@@ -87,13 +91,13 @@ export class PlayerComponent implements OnInit, OnDestroy {
 
   nextSearchType() {
     this.searchTypeIndex = (this.searchTypeIndex + 1) % this.searchTypes.length;
-    this.search.nativeElement.value = '';
+    this.searchInput.nativeElement.value = '';
     this.getFilteredPlaylist();
   }
 
   searchAlbum(album: string) {
     this.searchTypeIndex = this.searchTypes.findIndex(item => item === 'Album');
-    this.search.nativeElement.value = album;
+    this.searchInput.nativeElement.value = album;
     this.getFilteredPlaylist(album);
   }
 
@@ -101,21 +105,68 @@ export class PlayerComponent implements OnInit, OnDestroy {
     if (this.filteredPlaylist && this.filteredPlaylist.length) {
       this.currentSong = this.filteredPlaylist[0];
       this.currentSongIndex = this.currentPlaylist.songs.findIndex(item => item === this.currentSong);
-      this.search.nativeElement.value = '';
+      this.play(true);
+      this.searchInput.nativeElement.value = '';
       this.getFilteredPlaylist();
     }
   }
 
-  play() {
-    if (this.playing) {
-      this.audio.nativeElement.pause();
+  play(reset?: boolean) {
+    if (reset) {
+      this.playing = false;
+      this.audioElement.nativeElement.pause();
+      this.currentPlayingSong = null;
+      this.play();
     } else {
-      if (!this.currentPlayingSong) {
-        this.currentPlayingSong = this.currentSong;
-        this.audio.nativeElement.src = this.currentPlayingSong.url;
+      if (this.playing) {
+        this.audioElement.nativeElement.pause();
+      } else {
+        if (!this.currentPlayingSong) {
+          this.currentPlayingSong = this.currentSong;
+          this.audioElement.nativeElement.src = this.currentPlayingSong.url;
+        }
+        this.audioElement.nativeElement.play();
       }
-      this.audio.nativeElement.play();
+      this.playing = !this.playing;
     }
-    this.playing = !this.playing;
+  }
+
+  songClick(song: Song) {
+    this.selectSong(song);
+    if (this.dblClickFlag) {
+      this.dblClickFlag = false;
+      this.play(true);
+    } else {
+      this.dblClickFlag = true;
+      setTimeout(() => {
+        if (this.dblClickFlag) {
+          this.dblClickFlag = false;
+        }
+      }, 333);
+    }
+  }
+
+  songPlaying(song: Song): boolean {
+    return this.playing && song && this.currentPlayingSong ? song.name === this.currentPlayingSong.name : false;
+  }
+
+  toggleShuffle(filter: string) {
+    this.shuffle = !this.shuffle;
+    if (this.shuffle) {
+      if (this.currentPlaylist && this.currentPlaylist.songs) {
+        const result = [];
+        this.currentPlaylist.songs.forEach(song => {
+          result.push({ r: Math.random(), song});
+        });
+        this.currentPlaylist.songs = result.sort((a, b) => a.r - b.r).map(item => item.song);
+        if (this.currentPlayingSong) {
+          this.currentPlaylist.songs = this.currentPlaylist.songs.filter(song => song.name !== this.currentPlayingSong.name);
+          this.currentPlaylist.songs.unshift(this.currentPlayingSong);
+        }
+      }
+    } else {
+      this.currentPlaylist = JSON.parse(JSON.stringify(this.currentPlaylistUnshuffled));
+    }
+    this.getFilteredPlaylist(filter);
   }
 }
